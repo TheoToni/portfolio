ff13fd5b78d2cb0a824ba9d7d3c6ffaa2762af65
TheoToni <32997269+TheoToni@users.noreply.github.com>
2025-03-12 16:33:27 +0100

    homepage background glow effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
 export default function Home() {
   return (
     <main className="h-[calc(100vh-13rem)] relative flex flex-col items-center justify-center overflow-hidden w-full">
-      {/* Glowing ball */}
       <div
-        className="absolute w-64 h-64 right-[-10rem] rounded-full bg-emerald-300 animate-pulse"
+        className="absolute "
         style={{
           boxShadow:
-            "0 0 60px 30px rgba(110, 231, 183, 0.3), 0 0 100px 60px rgba(110, 231, 183, 0.3), 0 0 140px 90px rgba(110, 231, 183, 0.3)",
+            "60px 60px 150px 100px rgba(110, 231, 183, 0.15), 0 0 200px 120px rgba(110, 231, 183, 0.15), 0 0 250px 150px rgba(110, 231, 183, 0.15)",
         }}
       ></div>
-      <div className="absolute right-48 w-3 h-3  rounded-full bg-emerald-300 animate-pulse"></div>
-      <div className="absolute right-95 top-80 w-2 h-2  rounded-full bg-emerald-300 animate-pulse"></div>
-      <div className="absolute right-60 top-48 w-2 h-2  rounded-full bg-emerald-300 animate-pulse"></div>
 
-      {/* Content can go here */}
       <div className="max-w-4xl w-full px-4">
         <div className="z-10 text-white">test</div>
       </div>
